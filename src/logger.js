d9c06482aeaf1d40f8c7e23e915eaf40008a646a
Lars Willighagen <lars.willighagen@gmail.com>
2018-01-06 14:46:21 +0100

    [log] Don't use stream.Writable
    Fix #124

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,11 @@
-import {Writable} from 'stream'
-
-// If testing from CLI, use noop logger, to not interfere with the mocha reporter
-// (see issues #1998, #2107, etc. at https://github.com/mochajs/mocha)
 if (process.env.MOCHA === '1') {
-  global.logger = new console.Console(new Writable)
+  // If testing from CLI, use noop logger, to not interfere with the mocha reporter
+  // (see issues mochajs/mocha#1998, mochajs/mocha#2107, etc.)
+
+  // code only uses these methods, so this should be enough
+  global.logger = {error () {}, warn () {}, info () {}}
 } else {
+  // Else make a stderr-only console, so that you can redirect the CLI output to a
+  // file (see issue #73)
   global.logger = new console.Console(process.stderr)
 }
