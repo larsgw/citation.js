7c475b614577991ec10a992261a457ea0116de7b
Lars Willighagen <lars.willighagen@gmail.com>
2018-04-30 01:05:50 +0200

    [site:test] Fix browser tests again
    expect.js doesn't play nicely with Babel, so it
    shouldn't be transformed by Babelify.^1 Note that
    the ignore option in Babelify is buggy ^2, so
    this might not work on your end.
    
    ^1 https://github.com/Automattic/expect.js/issues/149
    ^2 https://github.com/babel/babelify/issues/265

--- tools/build-test.js
+++ tools/build-test.js
@@ -6,6 +6,9 @@ var babelify = require('babelify')
 browserify()
   .exclude(['citation-js'])
   .add('./test/wrapper.js')
-  .transform(babelify, {global: true})
+  .transform(babelify, {
+    global: true,
+    ignore: ['node_modules/expect.js/']
+  })
   .bundle()
   .pipe(fs.createWriteStream(path.join(__dirname, '../build/test.citation.js')))
