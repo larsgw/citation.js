e45ae489721f203c06407ebba9a99888c13cec3f
Lars Willighagen <lars.willighagen@gmail.com>
2018-05-03 22:52:36 +0200

    [test:input] Fix sync-request logging workaround
    Because the logging interfered with the Mocha
    reporter, the server was usually started before
    the first test. However, this doesn't make sense
    for tests were the server isn't used.

--- test/citation.js
+++ test/citation.js
@@ -3,6 +3,9 @@ if (process.env.TEST_MOCK_HTTP !== 'false') {
   const {fetchFile, fetchFileAsync} = require('./api')
   mock('../src/util/fetchFile.js', {default: fetchFile, __esModule: true})
   mock('../src/util/fetchFileAsync.js', {default: fetchFileAsync, __esModule: true})
+} else {
+  // start sync-request beforehand (interferes with the reporter otherwise)
+  try { require('sync-request')() } catch (e) { }
 }
 
 module.exports = require('../src/index')
