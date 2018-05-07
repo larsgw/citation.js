a5a6efb6073d299cbb3f0f53f3b36a9fbd68b9f2
Lars Willighagen <lars.willighagen@gmail.com>
2018-05-07 23:46:49 +0200

    [input:date] Rework Date parser (#139)
    Rework date parser to stabilise supported formats
    and to be able to determine date precision (i.e.
    year, month or day) for CSL. This can't be done
    with the native Date API, as aptly pointed out by
    several users on SO.
    
    Add test cases for the new Date parser.
    
    Close #138
    See #127
    See https://stackoverflow.com/q/49771378/5095300

--- src/parse/date.js
+++ src/parse/date.js
@@ -1,18 +1,310 @@
 /**
- * Convert epoch to CSL date
+ * Maps of month indexes by month name
+ * @default
+ */
+const monthMap = {
+  jan: 1,
+  feb: 2,
+  mar: 3,
+  apr: 4,
+  may: 5,
+  jun: 6,
+  jul: 7,
+  aug: 8,
+  sep: 9,
+  oct: 10,
+  nov: 11,
+  dec: 12
+}
+
+/**
+ * Get month index from month name
+ *
+ * @param {String} monthName - Name of the given month (in English), or abbreviations
+ * @return {Number} month index
+ * @return {undefined}
+ */
+const getMonth = monthName => monthMap[monthName.toLowerCase().slice(0, 3)]
+
+/**
+ * Get date parts from epoch time
+ *
+ * @param {Number} date - epoch time
+ * @return {dateParts} if valid epoch time
+ * @return {null} else null
+ */
+const parseEpoch = function (date) {
+  let epoch = new Date(date)
+
+  if (typeof date === 'number' && !isNaN(epoch.valueOf())) {
+    return [epoch.getFullYear(), epoch.getMonth() + 1, epoch.getDate()]
+  } else {
+    return null
+  }
+}
+
+/**
+ * Get date parts from ISO-8601 time. Formats supported:
+ *
+ *   * YYYY-MM-DD
+ *   * [+-]YYYYYY[Y...]-MM-DD
+ *
+ * Time parts are supported but disregarded.
+ *
+ * @param {String} date - ISO-8601 time
+ * @return {dateParts} if valid ISO-8601 time
+ * @return {null} else null
+ */
+const parseIso8601 = function (date) {
+  const pattern = /^(\d{4}|[-+]\d{6,})-(\d{2})-(\d{2})/
+
+  if (typeof date !== 'string' || !pattern.test(date)) {
+    return null
+  }
+
+  let [, year, month, day] = date.match(pattern)
+
+  if (!+month) {
+    return [year]
+  } else if (!+day) {
+    return [year, month]
+  } else {
+    return [year, month, day]
+  }
+}
+
+/**
+ * Get date parts from RFC-2822 time. Formats supported:
+ *
+ *   * [DDD, ]DD MMM YYYY
+ *
+ * Where DDD denotes week day name and MMM denotes month name.
+ *
+ * Time parts are supported but disregarded.
+ *
+ * @param {String} date - RFC-2822 time
+ * @return {dateParts} if valid RFC-2822 time
+ * @return {null} else null
+ */
+const parseRfc2822 = function (date) {
+  const pattern = /^(?:[a-z]{3},\s*)?(\d{1,2}) ([a-z]{3}) (\d{4,})/i
+
+  if (typeof date !== 'string' || !pattern.test(date)) {
+    return null
+  }
+
+  let [, day, month, year] = date.match(pattern)
+
+  month = getMonth(month)
+  if (!month) {
+    return null
+  }
+
+  return [year, month, day]
+}
+
+/**
+ * Get date-parts from a classical American date with day-precision. Formats supported:
+ *
+ *   * M[M]/D[D]/YY[YY]
+ *
+ * Note that the last case mandates a two- or four-digit year.
+ *
+ * The date is considered invalid if it doesn't exist according to the
+ * native `Date` implementation.
+ *
+ * @param {String} date - day time
+ * @return {dateParts} if valid day time
+ * @return {null} else null
+ */
+const parseAmericanDay = function (date) {
+  const pattern = /^(\d{1,2})\/(\d{1,2})\/(\d{2}(?:\d{2})?)/
+
+  if (typeof date !== 'string' || !pattern.test(date)) {
+    return null
+  }
+
+  let [, month, day, year] = date.match(pattern)
+
+  let check = new Date(year, month, day)
+  if (check.getMonth() === parseInt(month)) {
+    return [year, month, day]
+  } else {
+    return null
+  }
+}
+
+/**
+ * Get date-parts from a non-standard date with day-precision. Formats supported:
+ *
+ *   * D[D] M[M] [-]Y[Y...]
+ *   * D[D] MMM [-]Y[Y...]
+ *   * [-]Y[Y...] M[M] D[D]
+ *   * [-]Y[Y...] MMM D[D]
+ *
+ * Where MMM denotes month name.
+ *
+ * In all cases, " ", ".", "-" and "/" are all considered valid separators.
+ *
+ * This format prefers YY MM DD over DD MM YY and YY MMM DD over DD MMM YY
+ * if distinction is necessary. Note that in the overlap between these dates 
+ * the American date format ({@link parseAmericanDay}), the latter is
+ * preferred. This only happens when the separator is "/" and the date 
+ * would be valid.
+ *
+ * Any trailing parts (e.g. time parts) are supported but disregarded.
+ *
+ * @param {String} date - day time
+ * @return {dateParts} if valid day time
+ * @return {null} else null
+ */
+const parseDay = function (date) {
+  const pattern = /^(\d{1,2})[ .\-/](\d{1,2}|[a-z]{3,10})[ .\-/](-?\d+)/i
+  const reversePattern = /^(-?\d+)[ .\-/](\d{1,2}|[a-z]{3,10})[ .\-/](\d{1,2})/i
+
+  let year
+  let month
+  let day
+
+  if (typeof date !== 'string') {
+    return null
+  } else if (pattern.test(date)) {
+    [, day, month, year] = date.match(pattern)
+  } else if (reversePattern.test(date)) {
+    [, year, month, day] = date.match(reversePattern)
+  } else {
+    return null
+  }
+
+  if (getMonth(month)) {
+    month = getMonth(month)
+  } else if (isNaN(month)) {
+    return null
+  }
+
+  return [year, month, day]
+}
+
+/**
+ * Get date-parts from a non-standard date with month-precision. Formats supported:
+ *
+ *   * M[M] [-]Y[Y...]
+ *   * MMM [-]Y[Y...]
+ *   * [-]Y[Y...] M[M] (1)
+ *   * [-]Y[Y...] MMM
+ *
+ * Where MMM denotes month name.
+ *
+ * 1) since this format is ambigious with the first format, this is only
+ * assumed when the year is bigger than the month.
+ *
+ * In all cases, any sequence of non-alphanumerical characters is considered a valid
+ * separator.
+ *
+ * Trailing parts are **not** supported.
+ *
+ * @param {String} date - month time
+ * @return {dateParts} if valid month time
+ * @return {null} else null
+ */
+const parseMonth = function (date) {
+  const pattern = /^([a-z]{3,10}|-?\d+)[^\w-]+([a-z]{3,10}|-?\d+)$/i
+  if (typeof date === 'string' && pattern.test(date)) {
+    let values = date.match(pattern).slice(1, 3)
+
+    let month
+    if (getMonth(values[1])) {
+      month = getMonth(values.pop())
+    } else if (getMonth(values[0])) {
+      month = getMonth(values.shift())
+    } else if (values.some(isNaN) || values.every(value => +value < 0)) {
+      return null
+    } else if (+values[0] < 0) {
+      month = values.pop()
+    } else if (+values[0] > +values[1] && +values[1] > 0) {
+      month = values.pop()
+    } else {
+      month = values.shift()
+    }
+
+    let year = values.pop()
+
+    return [year, month]
+  } else {
+    return null
+  }
+}
+
+/**
+ * Get date-parts from a non-standard date with year-precision. Formats supported:
+ *
+ *   * [-]Y[Y...]
+ *
+ * Trailing parts are **not** supported.
+ *
+ * @param {String} date - year time
+ * @return {dateParts} if valid year time
+ * @return {null} else null
+ */
+const parseYear = function (date) {
+  if (typeof date === 'string' && /^-?\d+$/.test(date)) {
+    return [date]
+  } else {
+    return null
+  }
+}
+
+/**
+ * Convert date to CSL date. Supported formats:
+ *
+ *   * Epoch time (in number form)
+ *   * `YYYY-MM-DD`
+ *   * `[+-]YYYYYY[Y...]-MM-DD`
+ *   * `[DDD, ]DD MMM YYYY`
+ *   * `M[M]/D[D]/YY[YY]       (1)`
+ *   * `D[D] M[M] Y[Y...]      (2, 1)`
+ *   * `[-]Y[Y...] M[M] D[D]   (2)`
+ *   * `D[D] MMM Y[Y...]       (2)`
+ *   * `[-]Y[Y...] MMM D[D]    (2)`
+ *   * `M[M] [-]Y[Y...]        (3, 5)`
+ *   * `[-]Y[Y...] M[M]        (3, 4, 5)`
+ *   * `MMM [-]Y[Y...]         (3, 5)`
+ *   * `[-]Y[Y...] MMM         (3, 5)`
+ *   * `[-]Y[Y...]             (5)`
+ *
+ * Generally, formats support trailing parts, which are disregarded.
+ *
+ *   1. When the former of these formats overlaps with the latter, the
+ *      former is preferred
+ *   2. " ", ".", "-" and "/" are all supported as separator
+ *   3. Any sequence of non-alphanumerical characters are supported as 
+ *      separator
+ *   4. This format is only assumed if the year is bigger than the month
+ *   5. This format doesn't support trailing parts
  *
  * @access protected
  * @memberof Cite.parse
  *
- * @param {Number|String} value - Epoch time or string in format "YYYY-MM-DD"
+ * @param {Number|String} value - date in supported format, see above
  *
  * @return {Object} Object with property "date-parts" with the value [[ YYYY, MM, DD ]]
+ * @return {Object} If unparsable, object with property "raw" with the inputted value
  */
 const parseDate = function (value) {
-  const date = new Date(value)
-  return date.getFullYear()
-    ? {'date-parts': [[date.getFullYear(), date.getMonth() + 1, date.getDate()]]}
-    : {'raw': value}
+  let dateParts = parseEpoch(value) ||
+                  parseIso8601(value) ||
+                  parseRfc2822(value) ||
+                  parseAmericanDay(value) ||
+                  parseDay(value) ||
+                  parseMonth(value) ||
+                  parseYear(value)
+
+  if (dateParts) {
+    dateParts = dateParts.map(string => parseInt(string))
+    return {'date-parts': [dateParts]}
+  } else {
+    return {raw: value}
+  }
 }
 
 export const scope = '@date'
--- test/input.spec.js
+++ test/input.spec.js
@@ -136,6 +136,153 @@ describe('input', function () {
     }
   })
 
+  describe('date parser', function () {
+    const parse = Cite.parse.date
+    describe('epoch time', function () {
+      it('works', function () {
+        expect(parse(0)).to.eql({'date-parts': [[1970, 1, 1]]})
+        expect(parse(1500000000000)).to.eql({'date-parts': [[2017, 7, 14]]})
+      })
+      it('ignores non-numerical values', function () {
+        expect(parse('1500000000000')).not.to.eql({'date-parts': [[2017, 7, 14]]})
+      })
+    })
+    describe('ISO-8601 time', function () {
+      it('works for short dates', function () {
+        expect(parse('0000-01-01')).to.eql({'date-parts': [[0, 1, 1]]})
+        expect(parse('2000-01-01')).to.eql({'date-parts': [[2000, 1, 1]]})
+        expect(parse('9999-01-01')).to.eql({'date-parts': [[9999, 1, 1]]})
+      })
+      it('works for long dates', function () {
+        expect(parse('000000-01-01')).to.eql({'date-parts': [[0, 1, 1]]})
+        expect(parse('002000-01-01')).to.eql({'date-parts': [[2000, 1, 1]]})
+        expect(parse('200000-01-01')).to.eql({'date-parts': [[200000, 1, 1]]})
+        expect(parse('+002000-01-01')).to.eql({'date-parts': [[2000, 1, 1]]})
+        expect(parse('-002000-01-01')).to.eql({'date-parts': [[-2000, 1, 1]]})
+      })
+      it('works for super long dates', function () {
+        expect(parse(`${13e7}-01-01`)).to.eql({'date-parts': [[13e7, 1, 1]]})
+      })
+      it('disregards time values', function () {
+        expect(parse('2000-01-01T20:20:20')).to.eql({'date-parts': [[2000, 1, 1]]})
+      })
+      it('works for different precisions', function () {
+        expect(parse('2000-01-01')).to.eql({'date-parts': [[2000, 1, 1]]})
+        expect(parse('2000-01-00')).to.eql({'date-parts': [[2000, 1]]})
+        expect(parse('2000-00-00')).to.eql({'date-parts': [[2000]]})
+      })
+    })
+    describe('RFC-2822 time', function () {
+      it('works', function () {
+        expect(parse('1 Jan 0000')).to.eql({'date-parts': [[0, 1, 1]]})
+        expect(parse('5 Feb 2000')).to.eql({'date-parts': [[2000, 2, 5]]})
+        expect(parse('12 Mar 20001')).to.eql({'date-parts': [[20001, 3, 12]]})
+      })
+      it('works with week days', function () {
+        expect(parse('Tue, 23 May 0000')).to.eql({'date-parts': [[0, 5, 23]]})
+        expect(parse('Fri, 13 Apr 2001')).to.eql({'date-parts': [[2001, 4, 13]]})
+        expect(parse('Wed, 30 Jun 20001')).to.eql({'date-parts': [[20001, 6, 30]]})
+      })
+      it('disregards time values', function () {
+        expect(parse('Sat, 1 Jan 2000 20h20m20s')).to.eql({'date-parts': [[2000, 1, 1]]})
+      })
+    })
+    describe('non-standard time', function () {
+      describe('with day-precision', function () {
+        it('works', function () {
+          expect(parse('1 1 2000')).to.eql({'date-parts': [[2000, 1, 1]]})
+          expect(parse('1 Jan 2000')).to.eql({'date-parts': [[2000, 1, 1]]})
+          expect(parse('01 01 2000')).to.eql({'date-parts': [[2000, 1, 1]]})
+          expect(parse('01 Jan 2000')).to.eql({'date-parts': [[2000, 1, 1]]})
+          expect(parse('1 1 -2000')).to.eql({'date-parts': [[-2000, 1, 1]]})
+          expect(parse('1 Jan -2000')).to.eql({'date-parts': [[-2000, 1, 1]]})
+        })
+        it('works reversed', function () {  
+          expect(parse('2000 1 1')).to.eql({'date-parts': [[2000, 1, 1]]})
+          expect(parse('2000 Jan 1')).to.eql({'date-parts': [[2000, 1, 1]]})
+          expect(parse('2000 01 01')).to.eql({'date-parts': [[2000, 1, 1]]})
+          expect(parse('2000 Jan 01')).to.eql({'date-parts': [[2000, 1, 1]]})
+          expect(parse('-2000 1 1')).to.eql({'date-parts': [[-2000, 1, 1]]})
+          expect(parse('-2000 Jan 1')).to.eql({'date-parts': [[-2000, 1, 1]]})
+        })
+        it('disregards time values', function () {
+          expect(parse('2000.01.01 20:20:20')).to.eql({'date-parts': [[2000, 1, 1]]})
+        })
+        context('and different separators like', function () {
+          it('"." work', function () {
+            expect(parse('1.1.2000')).to.eql({'date-parts': [[2000, 1, 1]]})
+          })
+          it('"-" work', function () {
+            expect(parse('1-1-2000')).to.eql({'date-parts': [[2000, 1, 1]]})
+          })
+          it('"/" work', function () {
+            expect(parse('1/1/2000')).to.eql({'date-parts': [[2000, 1, 1]]})
+          })
+        })
+        context('and American formatting', function () {
+          it('works', function () {
+            expect(parse('5/2/2000')).to.eql({'date-parts': [[2000, 5, 2]]})
+            expect(parse('5/2/20')).to.eql({'date-parts': [[20, 5, 2]]})
+          })
+          it('ignores invalid dates', function () {
+            expect(parse('30/5/2000')).to.eql({'date-parts': [[2000, 5, 30]]})
+          })
+          it('ignores dates with other separators', function () {
+            expect(parse('5 2 2000')).not.to.eql({'date-parts': [[2000, 5, 2]]})
+          })
+          it('disregards time values', function () {
+            expect(parse('1/1/2000 20:20:20')).to.eql({'date-parts': [[2000, 1, 1]]})
+          })
+        })
+      })
+      describe('with month-precision', function () {
+        it('works', function () {
+          expect(parse('Jan 2000')).to.eql({'date-parts': [[2000, 1]]})
+          expect(parse('2000 Jan')).to.eql({'date-parts': [[2000, 1]]})
+          expect(parse('January 2000')).to.eql({'date-parts': [[2000, 1]]})
+          expect(parse('Jan -2000')).to.eql({'date-parts': [[-2000, 1]]})
+          expect(parse('-2000 Jan')).to.eql({'date-parts': [[-2000, 1]]})
+        })
+        it('works when both values are numbers', function () {
+          expect(parse('1 2000')).to.eql({'date-parts': [[2000, 1]]})
+          expect(parse('2000 1')).to.eql({'date-parts': [[2000, 1]]})
+        })
+        it('works when one value is negative', function () {
+          expect(parse('1 -2000')).to.eql({'date-parts': [[-2000, 1]]})
+          expect(parse('-2000 1')).to.eql({'date-parts': [[-2000, 1]]})
+        })
+        it('defaults to MM YY', function () {
+          expect(parse('1 2')).to.eql({'date-parts': [[2, 1]]})
+          expect(parse('1 -2')).to.eql({'date-parts': [[-2, 1]]})
+        })
+      })
+      describe('with year-precision', function () {
+        it('works', function () {
+          expect(parse('2000')).to.eql({'date-parts': [[2000]]})
+          expect(parse('-2000')).to.eql({'date-parts': [[-2000]]})
+        })
+      })
+    })
+    describe('invalid time', function () {
+      it('works for non-strings and non-numbers', function () {
+        expect(parse()).to.eql({raw: undefined})
+      })
+      it('works for invalid month names', function () {
+        let inputs = ['2000 naj 1', '1 naj 2000', 'naj 2000', '2000 naj']
+        for (let input of inputs) {
+          expect(parse(input)).to.have.property('raw', input)
+        }
+      })
+      it('works for invalid strings', function () {
+        expect(parse('foo')).to.have.property('raw', 'foo')
+      })
+      it('works for invalid numbers', function () {
+        expect(parse(NaN)).to.have.property('raw')
+        expect(parse(Infinity)).to.eql({raw: Infinity})
+      })
+    })
+  })
+
   describe('interface', function () {
     const ref = '@test'
     const type = `${ref}/foo`
