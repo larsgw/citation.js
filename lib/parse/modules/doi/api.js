larsgw/citation.js

commit 509d911836c7cb7adbc57440ce21a703308e01ff

[input:file] Generalise request methods

lib/parse/modules/doi/api.js
@@ -5,44 +5,42 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.parseAsync = exports.parse = exports.types = exports.scope = void 0;
 
-var _syncRequest = _interopRequireDefault(require("sync-request"));
-
 var _json = _interopRequireDefault(require("./json"));
+
+var _fetchFile = _interopRequireDefault(require("../../../util/fetchFile"));
+
+var _fetchFileAsync = _interopRequireDefault(require("../../../util/fetchFileAsync"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } } function _next(value) { step("next", value); } function _throw(err) { step("throw", err); } _next(); }); }; }
 
+var apiHeaders = {
+  Accept: 'application/vnd.citationstyles.csl+json'
+};
+
 var fetchDoiApiAsync = function () {
   var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(url) {
-    var headers;
+    var result;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _context.prev = 0;
-            headers = new Headers();
-            headers.append('Accept', 'application/vnd.citationstyles.csl+json');
-            _context.next = 5;
-            return fetch(url, {
-              headers: headers
+            _context.next = 2;
+            return (0, _fetchFileAsync.default)(url, {
+              headers: apiHeaders
             });
 
-          case 5:
-            return _context.abrupt("return", _context.sent.json());
+          case 2:
+            result = _context.sent;
+            return _context.abrupt("return", result === '[]' ? result : {});
 
-          case 8:
-            _context.prev = 8;
-            _context.t0 = _context["catch"](0);
-            logger.error('[set]', "File '".concat(url, "' could not be fetched:"), _context.t0.message);
-            return _context.abrupt("return", {});
-
-          case 12:
+          case 4:
           case "end":
             return _context.stop();
         }
       }
-    }, _callee, this, [[0, 8]]);
+    }, _callee, this);
   }));
 
   return function fetchDoiApiAsync(_x) {
@@ -80,17 +78,10 @@ var parseDoiApiAsync = function () {
 exports.parseAsync = parseDoiApiAsync;
 
 var fetchDoiApi = function fetchDoiApi(url) {
-  try {
-    return JSON.parse((0, _syncRequest.default)('GET', url, {
-      headers: {
-        Accept: 'application/vnd.citationstyles.csl+json'
-      },
-      allowRedirectHeaders: ['Accept']
-    }).getBody('utf8'));
-  } catch (e) {
-    logger.error('[set]', "File '".concat(url, "' could not be fetched:"), e.message);
-    return {};
-  }
+  var result = (0, _fetchFile.default)(url, {
+    headers: apiHeaders
+  });
+  return result === '[]' ? result : {};
 };
 
 var parseDoiApi = function parseDoiApi(data) {

src/parse/modules/doi/api.js
@@ -2,10 +2,18 @@
  * @module input/doi
  */
 
-import request from 'sync-request'
 import parseDoiJson from './json'
+import fetchFile from '../../../util/fetchFile'
+import fetchFileAsync from '../../../util/fetchFileAsync'
 
-/* global fetch, Headers */
+/**
+ * DOI API headers
+ *
+ * @access private
+ */
+const apiHeaders = {
+  Accept: 'application/vnd.citationstyles.csl+json'
+}
 
 /**
  * Fetch DOI API results
@@ -18,14 +26,8 @@ import parseDoiJson from './json'
  * @return {Promise<CSL>} The fetched JSON
  */
 const fetchDoiApiAsync = async function (url) {
-  try {
-    const headers = new Headers()
-    headers.append('Accept', 'application/vnd.citationstyles.csl+json')
-    return (await fetch(url, {headers})).json()
-  } catch (e) {
-    logger.error('[set]', `File '${url}' could not be fetched:`, e.message)
-    return {}
-  }
+  const result = await fetchFileAsync(url, {headers: apiHeaders})
+  return result === '[]' ? result : {}
 }
 
 /**
@@ -54,17 +56,8 @@ const parseDoiApiAsync = async function (data) {
  * @return {CSL} The fetched JSON
  */
 const fetchDoiApi = function (url) {
-  try {
-    return JSON.parse(request('GET', url, {
-      headers: {
-        Accept: 'application/vnd.citationstyles.csl+json'
-      },
-      allowRedirectHeaders: ['Accept']
-    }).getBody('utf8'))
-  } catch (e) {
-    logger.error('[set]', `File '${url}' could not be fetched:`, e.message)
-    return {}
-  }
+  const result = fetchFile(url, {headers: apiHeaders})
+  return result === '[]' ? result : {}
 }
 
 /**

lib/util/fetchFile.js
@@ -10,8 +10,16 @@ var _syncRequest = _interopRequireDefault(require("sync-request"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var fetchFile = function fetchFile(url) {
+  var opts = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  var reqOpts = {};
+
+  if (opts.headers) {
+    reqOpts.headers = opts.headers;
+    reqOpts.allowRedirectHeaders = Object.keys(opts.headers);
+  }
+
   try {
-    return (0, _syncRequest.default)('GET', url).getBody('utf8');
+    return (0, _syncRequest.default)('GET', url, reqOpts).getBody('utf8');
   } catch (e) {
     logger.error('[set]', "File '".concat(url, "' could not be fetched:"), e.message);
     return '[]';

lib/util/fetchFileAsync.js
@@ -11,29 +11,38 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
 var fetchFileAsync = function () {
   var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(url) {
+    var opts,
+        reqOpts,
+        _args = arguments;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _context.prev = 0;
-            _context.next = 3;
-            return fetch(url);
+            opts = _args.length > 1 && _args[1] !== undefined ? _args[1] : {};
+            reqOpts = {};
 
-          case 3:
-            return _context.abrupt("return", _context.sent.text());
+            if (opts.headers) {
+              reqOpts.headers = opts.headers;
+              reqOpts.allowRedirectHeaders = Object.keys(opts.headers);
+            }
 
-          case 6:
-            _context.prev = 6;
-            _context.t0 = _context["catch"](0);
+            _context.prev = 3;
+            return _context.abrupt("return", fetch(url, reqOpts).then(function (response) {
+              return response.text();
+            }));
+
+          case 7:
+            _context.prev = 7;
+            _context.t0 = _context["catch"](3);
             logger.error('[set]', "File '".concat(url, "' could not be fetched:"), _context.t0.message);
             return _context.abrupt("return", '[]');
 
-          case 10:
+          case 11:
           case "end":
             return _context.stop();
         }
       }
-    }, _callee, this, [[0, 6]]);
+    }, _callee, this, [[3, 7]]);
   }));
 
   return function fetchFileAsync(_x) {

src/util/fetchFile.js
@@ -7,12 +7,19 @@ import request from 'sync-request'
  * @memberof Cite.util
  *
  * @param {String} url - The input url
+ * @param {Object} opts - Request options
  *
  * @return {String} The fetched string
  */
-const fetchFile = function (url) {
+const fetchFile = function (url, opts = {}) {
+  const reqOpts = {}
+  if (opts.headers) {
+    reqOpts.headers = opts.headers
+    reqOpts.allowRedirectHeaders = Object.keys(opts.headers)
+  }
+
   try {
-    return request('GET', url).getBody('utf8')
+    return request('GET', url, reqOpts).getBody('utf8')
   } catch (e) {
     logger.error('[set]', `File '${url}' could not be fetched:`, e.message)
     return '[]'

src/util/fetchFileAsync.js
@@ -9,12 +9,19 @@ import 'isomorphic-fetch'
  * @memberof Cite.util
  *
  * @param {String} url - The input url
+ * @param {Object} opts - Request options
  *
  * @return {Promise<String>} The fetched string
  */
-const fetchFileAsync = async function (url) {
+const fetchFileAsync = async function (url, opts = {}) {
+  const reqOpts = {}
+  if (opts.headers) {
+    reqOpts.headers = opts.headers
+    reqOpts.allowRedirectHeaders = Object.keys(opts.headers)
+  }
+
   try {
-    return (await fetch(url)).text()
+    return fetch(url, reqOpts).then(response => response.text())
   } catch (e) {
     logger.error('[set]', `File '${url}' could not be fetched:`, e.message)
     return '[]'

commit dda2360f4d91af1d48e1bf859d784b2bdc694c6d

[input:doi] Fix horrendous DOI bug
Introduced in 509d911, the general fetchFile API
is pretty nice, unless you do stupid things, like
forgetting to parse the results as JSON, breaking
all of DOI.

lib/parse/modules/doi/api.js
@@ -33,7 +33,7 @@ var fetchDoiApiAsync = function () {
 
           case 2:
             result = _context.sent;
-            return _context.abrupt("return", result === '[]' ? result : {});
+            return _context.abrupt("return", result === '[]' ? {} : JSON.parse(result));
 
           case 4:
           case "end":
@@ -81,7 +81,7 @@ var fetchDoiApi = function fetchDoiApi(url) {
   var result = (0, _fetchFile.default)(url, {
     headers: apiHeaders
   });
-  return result === '[]' ? result : {};
+  return result === '[]' ? {} : JSON.parse(result);
 };
 
 var parseDoiApi = function parseDoiApi(data) {

src/parse/modules/doi/api.js
@@ -27,7 +27,7 @@ const apiHeaders = {
  */
 const fetchDoiApiAsync = async function (url) {
   const result = await fetchFileAsync(url, {headers: apiHeaders})
-  return result === '[]' ? result : {}
+  return result === '[]' ? {} : JSON.parse(result)
 }
 
 /**
@@ -57,7 +57,7 @@ const parseDoiApiAsync = async function (data) {
  */
 const fetchDoiApi = function (url) {
   const result = fetchFile(url, {headers: apiHeaders})
-  return result === '[]' ? result : {}
+  return result === '[]' ? {} : JSON.parse(result)
 }
 
 /**